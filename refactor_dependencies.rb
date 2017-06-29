f49bb03430a58b4ce020247a3565eb82db33c17e
Elise Adams <neuman.elise@gmail.com>
2017-06-29 15:11:06 -0600

    Inital commit

---
+++ refactor_dependencies.rb
@@ -0,0 +1,17 @@
+array = [
+ "KittenService: ",
+ "Leetmeme: Cyberportal",
+ "Cyberportal: Ice",
+ "CamelCaser: KittenService",
+ "Fraudstream: Leetmeme",
+ "Ice: "
+]
+
+@packages = []
+@packages_dependency = []
+
+def dependencies_challenge
+ puts "hello"
+end
+
+dependencies_challenge
